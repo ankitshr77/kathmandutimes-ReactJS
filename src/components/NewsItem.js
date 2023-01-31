19fe05d7715c85a3c5f34e817b3fbe81c3f427d0
ankitshr77 <ankitshr77@gmail.com>
2023-01-31 14:13:46 +0545

    tutorial 27

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
     return (
       
         <div className="card mt-5" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/zESdDhnu0VA8kUpWh0mtaBZ-nUI=/1200x630/filters:focal(2120x64:2130x54):no_upscale()/cloudfront-us-east-1.images.arcpublishing.com/dmn/XPMOY3ORAZDRTOIRIEKUDAIZHA.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{desc}...</p>
